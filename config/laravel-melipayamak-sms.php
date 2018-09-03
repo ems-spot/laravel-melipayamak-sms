9825b416ff1db653edf134935f20c7d37953d092
farid <farid.sh69@gmail.com>
2018-09-03 14:40:25 +0430

    removed domain

--- config/laravel-melipayamak-sms.php
+++ config/laravel-melipayamak-sms.php
@@ -10,7 +10,6 @@ return [
 	
 	'username' => env('MELIPAYAMAK_USERNAME'),
 	'password' => env('MELIPAYAMAK_PASSWORD'),
-	'domain' => env('MELIPAYAMAK_DOMAIN'),
 	'from' => env('MELIPAYAMAK_FROM'),
 	'url' => env('MELIPAYAMAK_BASE_HTTP_URL', 'http://api.payamak-panel.com/post/Send.asmx?wsdl'),
 	'error_max_value' => env('MELIPAYAMAK_ERROR_MAX_VALUE', '1000'),
